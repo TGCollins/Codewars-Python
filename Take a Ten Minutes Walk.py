5cc7ddceee35831fdc734dc0accd5ec9b3a5b614
TGCollins <106698921+TGCollins@users.noreply.github.com>
2022-06-30 22:19:16 +0100

    Delete Take a Ten Minutes Walk.py

--- Take a Ten Minutes Walk.py
+++
@@ -1,8 +0,0 @@
-def is_valid_walk(walk):
-    if len(walk) == 10:
-        if walk.count("n") == walk.count("s") and walk.count("w") == walk.count("e"):
-            return print(True)
-        else:
-            return print(False)
-    else:
-        return print(False)
