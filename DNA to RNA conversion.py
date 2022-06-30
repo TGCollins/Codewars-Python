e18163f14852e5f90872bc7ba2d7f0b8b0c3b7ca
TGCollins <106698921+TGCollins@users.noreply.github.com>
2022-06-30 22:17:47 +0100

    Delete DNA to RNA conversion.py

--- DNA to RNA conversion.py
+++
@@ -1,8 +0,0 @@
-def dna_to_rna(dna):
-    translation = ""
-    for letter in dna:
-        if letter.upper() in "T":            
-            translation = translation + "U"
-        else:
-            translation = translation + letter
-    return translation
